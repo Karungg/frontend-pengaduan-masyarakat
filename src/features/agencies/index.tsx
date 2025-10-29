fb9bcab2b4a13c4134e18173039ab2277511c573
Karungg <miftahfadilah71@gmail.com>
2025-10-29 15:48:22 +0700

    feat: add agency user menu

---
+++ src/features/agencies/index.tsx
@@ -0,0 +1,47 @@
+import { getRouteApi } from '@tanstack/react-router'
+import { ConfigDrawer } from '@/components/config-drawer'
+import { Header } from '@/components/layout/header'
+import { Main } from '@/components/layout/main'
+import { ProfileDropdown } from '@/components/profile-dropdown'
+import { Search } from '@/components/search'
+import { ThemeSwitch } from '@/components/theme-switch'
+import { AdminDialogs } from './components/admins-dialogs'
+import { AdminPrimaryButtons } from './components/admins-primary-buttons'
+import { AdminProvider } from './components/admins-provider'
+import { AdminTable } from './components/admins-table'
+import { admins } from './data/admins'
+
+const route = getRouteApi('/_authenticated/admin/admins/')
+
+export function Admins() {
+  const search = route.useSearch()
+  const navigate = route.useNavigate()
+
+  return (
+    <AdminProvider>
+      <Header fixed>
+        <Search />
+        <div className='ms-auto flex items-center space-x-4'>
+          <ThemeSwitch />
+          <ConfigDrawer />
+          <ProfileDropdown />
+        </div>
+      </Header>
+
+      <Main className='flex flex-1 flex-col gap-4 sm:gap-6'>
+        <div className='flex flex-wrap items-end justify-between gap-2'>
+          <div>
+            <h2 className='text-2xl font-bold tracking-tight'>Admin List</h2>
+            <p className='text-muted-foreground'>
+              Manage your admins and their roles here.
+            </p>
+          </div>
+          <AdminPrimaryButtons />
+        </div>
+        <AdminTable data={admins} search={search} navigate={navigate} />
+      </Main>
+
+      <AdminDialogs />
+    </AdminProvider>
+  )
+}
